c509cb08b9b8d00a597dc31fa2606f7d7276d545
Ankit Kumar <88437909+ankit-daniel@users.noreply.github.com>
2021-09-17 17:32:47 +0530

    Add files via upload

---
+++ 4A - Watermelon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int w;
+    cin >> w;
+    if (w == 2 || w % 2 != 0)
+        cout << "NO";
+    else
+        cout << "YES";
+    return 0;
+}
