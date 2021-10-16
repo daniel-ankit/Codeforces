0b7f62142d8bc1a3761c0a33880a0f197ffbad15
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2021-10-16 23:44:54 +0530

    Add files via upload

---
+++ 1360A - Minimal Square.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<iomanip>
+using namespace std;
+
+#define TEST int T; cin >> T; while (T--)
+#define int long long 
+#define PB push_back
+#define PP pop_back
+#define MP make_pair
+#define MOD(n) n%1000000007
+#define VLL vector <long long>
+#define FOR(i, n) for(long long i=0; i<n; i++)
+#define SORT(Series) sort(Series.begin(), Series.end());
+#define Radhe ios_base::sync_with_stdio(false);
+#define Krishna cin.tie(NULL);
+
+int32_t main()
+{
+    Radhe Krishna
+    TEST
+    {
+        int A, B;
+        cin >> A >> B;
+        if(max(A, B)>=2*min(A, B)) cout << pow(max(A,B), 2);
+        else cout << pow(min(A,B)*2, 2);
+        cout << endl;
+    }
+    return 0;
+}
