62fd256a7ed0a19284009953fa881d474436ae6c
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2021-12-06 17:23:22 +0530

    Add files via upload

---
+++ 59B - Fortune Telling.cpp
@@ -0,0 +1,72 @@
+/*
+─────────────────────────────────────────────────────────────────────
+                  कोई अच्छा सा संस्कृति का श्लोक सोच लेना यहां                
+                 सब लिख रहे थे तो, मुझे लगा मैंडेटरी होगा लिखना..             
+─────────────────────────────────────────────────────────────────────
+*/
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<iomanip>
+using namespace std;
+
+#define TEST int T; cin >> T; while (T--)
+#define int long long 
+#define PB push_back
+#define PP pop_back
+#define MP make_pair
+#define MOD(n) n%1000000007
+#define VII vector <pair<int, int>>
+#define VI vector<int>
+#define VS vector<string>
+#define VC vector<char>
+#define VB vector<bool>
+#define FF first
+#define SS second
+#define LONGINT << fixed << setprecision(0)
+#define FOR(i, n) for(long long i=0; i<n; i++)
+#define Radhe ios_base::sync_with_stdio(false);
+#define Krishna cin.tie(NULL); cout.tie(NULL);
+#define VIN(X, N) for(int i=0; i<N; i++){int V; cin >> V; H.push_back(V);};
+#define VOUT(x,X) for(auto x:X) cout << x << " ";
+#define SORT(V) sort(V.begin(), V.end())
+#define REVERSE(V) reverse(V.begin(), V.end())
+#define ADD(V) accumulate(V.begin(), V.end(), 0)
+#define PI 3.1415926535897932384626433832795
+
+#define OLD  freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+long long lcm(int a, int b) {return (a / __gcd(a, b)) * b;}
+bool sortbysec(const pair<int,int> &a, const pair<int,int> &b){return (a.second < b.second);}
+int maxfreqof(vector<int>& nums) { int count=0, ans=0; for(int n:nums) { if(count==0) ans = n; (n==ans) ? (count++) : (count--); } return ans; }
+int MEX(set <int> V){ set<int> :: iterator j; int i=0; for (j=V.begin(); j!=V.end(); j++, i++) if(*j!=i) return i; return *(V.end())+1; }
+int maxfreq(vector<int> V) { int C=1, MAX=0; SORT(V); int pivot = V[0]; for(int i=1; i<V.size(); i++) { if(V[i]!=pivot) { pivot = V[i]; C=0; } C++; MAX = max(MAX, C); } return MAX; }
+
+int32_t main()
+{
+    Radhe Krishna
+    int N;
+    cin >> N;
+    int A[N];
+    for(int i=0; i<N; i++) cin >> A[i];
+    int S = accumulate(A, A+N, 0);
+    if(S%2!=0)
+    {
+        cout << S;
+        return 0;
+    }
+    sort(A, A+N);
+    int num=0, found = 0;
+    for(int i=0; i<N; i++)
+    {
+        if(A[i]%2!=0)
+        {
+            num = A[i];
+            found = 1;
+            break;
+        }
+    }
+    (found) ? (cout << S-num) : (cout << 0);
+    return 0;
+}
